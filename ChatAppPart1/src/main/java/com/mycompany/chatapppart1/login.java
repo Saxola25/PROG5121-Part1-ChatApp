d2caeb4f5fb1fd86b8e0e7ef22590c023609f082
Saxola25 <Skillowsaxola@gmail.com>
2026-09-17 13:14:38 +0200

    added cod eon main, added code on login ,added code on logintest.added comments

--- ChatAppPart1/src/main/java/com/mycompany/chatapppart1/login.java
+++ ChatAppPart1/src/main/java/com/mycompany/chatapppart1/login.java
@@ -1,134 +1,144 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.chatapppart1;
 
+/**
+ *The Login class handles the registration and login functionality
+ * of the ChatApp application
+ * @author Student
+ */
 public class Login {
 
-    // Declare variables that are going to be used by the user
-    String username;
-    String password;
-    String phoneNumber;
-    String firstName;
-    String lastName;
-    String enteredUsername;
-    String enteredPassword;
+    //Variables used to store the user's registration information
+    private String username;
+    private String password;
+    private String phoneNumber;
+
+    //Variables used to store the information entered during login
+    private String enteredUsername;
+    private String enteredPassword;
+
+    //Variables used to store the user's first and last name
+    private String firstName;
+    private String lastName;
 
     /*
-     * Constructor used to initialise the user's registration details.
-     * The values entered by the user are assigned to the class variables.
-     */
-    public Login(String firstName, String lastName, String username,
-                 String password, String phoneNumber) {
+    *Constructor used to initialise the user's registration details.
+    *The values entered by the user are assigned to the class variables.
+    */
 
+    public Login (String firstName, String lastName, String username, String password, String phoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
         this.phoneNumber = phoneNumber;
+
+    }
+    /*
+     * This method sets the user's password.
+     * It is used to store the password entered by the user
+     * after the Login object has been created.
+     */
+    public void setPassword(String password){
+        this.password = password;
     }
 
-    // Username Validation
-    public boolean checkUserName() {
-
-        if (username.contains("_") && username.length() <= 5) {
-            return true;
-        } else {
-            return false;
-        }
+    /*
+     * This method sets the user's cellphone number.
+     * It is used to store the cellphone number entered by
+     * the user after the Login object has been created.
+     */
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
     }
 
-    // Password Validation
-    public boolean checkPasswordComplexity() {
-
-        // Password must be at least 8 characters long
-        if (password.length() < 8) {
-            return false;
-        }
-
-        boolean hasUpperCase = false;
-        boolean hasNumber = false;
-        boolean hasSpecialCharacter = false;
-
-        for (int i = 0; i < password.length(); i++) {
-
-            char character = password.charAt(i);
-
-            if (Character.isUpperCase(character)) {
-                hasUpperCase = true;
-
-            } else if (Character.isDigit(character)) {
-                hasNumber = true;
-
-            } else if (!Character.isLetterOrDigit(character)) {
-                hasSpecialCharacter = true;
-            }
-        }
-
-        return hasUpperCase && hasNumber && hasSpecialCharacter;
+    /*
+     * This method returns the user's registered username.
+     * It allows other classes to access the username without
+     * directly accessing the private variable.
+     */
+    public String getUsername(){
+        return username;
     }
 
-    // Cellphone validation should start with +27
-    public boolean checkCellphoneNumber() {
-
-        if (phoneNumber.startsWith("+27")
-                && phoneNumber.length() == 12) {
-
-            for (int i = 3; i < phoneNumber.length(); i++) {
-
-                if (!Character.isDigit(phoneNumber.charAt(i))) {
-                    return false;
-                }
-            }
-
-            return true;
-
-        } else {
-            return false;
-        }
+    /*
+     * This method sets the username entered during login.
+     * It is used to store the username entered by the user
+     * when attempting to log in.
+     */
+    public void setEnteredUsername(String enteredUsername){
+        this.enteredUsername = enteredUsername;
     }
-    public String registerUser(){
-        String message = "";
-        if (checkUserName()){
-            message += "Username successfully captured.\n";
-        }else{
-            message+= "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than 5 characters.";
-            
-        }
-        if (checkPasswordComplexity()){
-            message += "password successfully captured.\n";
-        }else{
-            message+= "password is not correctly formatted; please ensure that your password contains atleast 8 characters, a capital letter, a special character, and a number ";
-            
-        }
-    if (checkCellphoneNumber() ){
-     message += "Cellphone number successfully added.\n";
-    }else{
-        message += "cellphone number is not correctly formatted or does not contain the international code: +27";
+
+    /*
+     * This method sets the password entered during login.
+     * It is used to store the password entered by the user
+     * when attempting to log in.
+     */
+    public void setEnteredPassword(String enteredPassword){
+        this.enteredPassword = enteredPassword;
     }
-    return message;
-    
+    /*
+     *This method checks whether the username is correctly formatted.
+     *The username must contain an underscore and must not be
+     *more than five characters long.
+     */
+    public boolean checkUserName(){
+    //Check whether the username contains an underscore
+    //and is no more than five characters long
+    return username.contains("_") && username.length()<=5;
     }
-    public boolean loginUser (){
-        if(enteredUsername.equals(username)
-                &&
-                enteredPassword.equals(password)){
-        return true;
-     }else{
-       return false;
-            }     
-                
-    }
-     public String returnloginStatus(){
-         if (loginUser()){
-             return "welcome " +firstName + " " + lastName + ",it is great to see you again";
-         }else{
-             return"Username or password is incorrect, please try again.";
-                     
-                     
+    /*
+     *This method checks whether the password meets the required
+     *complexity rules.
+     *
+     *The password must:
+     *-Have at least 8 characters
+     *-Contain an uppercase letter
+     *-Contain a number
+     *-Contain a special character
+     */
+
+     public boolean checkPasswordComplexity(){
+         //Boolean variables are used to keep track of whether
+         //each password requirement has been found.
+     boolean hasUppercase = false;
+     boolean hasNumber = false;
+     boolean hasSpecialCharacter = false;
+
+     //If the password has fewer than 8 characters,
+     //it is automatically invalid.
+     if (password.length()< 8){
+         return false;
+     }
+
+     //Loop through each character in the password.
+     for (int i = 0; i < password.length(); i++){
+
+         //Store the current character in the password.
+         char character = password.charAt(i);
+
+         //Check whether the character is an uppercase letter.
+         if (Character.isUpperCase(character)){
+             hasUppercase = true;
+         }
+
+         //Check whether the character is a number.
+         if (Character.isDigit(character)){
+             hasNumber = true;
+         }
+
+         //Check whether the character is not a letter or number.
+         //If so, it is considered a special character.
+         if (!Character.isLetterOrDigit(character)){
+             hasSpecialCharacter = true;
          }
      }
-}
+
+     //The password is valid only if all three requirements
+     //have been met.
+     if (hasUppercase && hasNumber && hasSp
     
 
